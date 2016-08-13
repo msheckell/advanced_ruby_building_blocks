817f5ade8e088393dedd94558b9f60126ea27a7a
Matt Sheckell <matt.sheckell@gmail.com>
2016-08-13 16:35:24 -0400

    Bubble Sort completed.

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(array)
+	complete = 0
+	until complete == array.length-1
+		complete = 0
+
+		(0..array.size-2).each_with_index do |num, index|
+			index_two = index+1
+			
+			if array[index] > array[index_two]
+				array[index], array[index_two] = array[index_two], array[index]
+			else
+				complete += 1
+			end
+		end
+	end
+	print array
+end
+
+bubble_sort([4,3,78,2,0,2])
