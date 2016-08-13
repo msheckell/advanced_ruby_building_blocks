cac5f3848710f2ecebdeb4b58372ae5f79ebb1d4
Matt Sheckell <matt.sheckell@gmail.com>
2016-08-13 17:13:14 -0400

    Completed bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -16,4 +16,28 @@ def bubble_sort(array)
 	print array
 end
 
+def bubble_sort_by(array)
+	complete = 0
+	until complete == array.length-1
+		complete = 0
+
+		(0..array.size-2).each_with_index do |num, index|
+			index_two = index+1
+			
+			if yield(array[index], array[index_two]) > 0
+				array[index], array[index_two] = array[index_two], array[index]
+			else
+				complete += 1
+			end
+		end
+	end
+	print array
+end
+
+
+
 bubble_sort([4,3,78,2,0,2])
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+	left.length - right.length
+end
